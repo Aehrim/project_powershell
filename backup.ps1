48b02b6bbc281bcac052c701f09cd131b4c4cc62
Rafael Hartung <63423283+Aehrim@users.noreply.github.com>
2024-03-05 09:42:23 +0100

    Properly Wrote the Backup Script Changes are Still needed Though

--- backup.ps1
+++ backup.ps1
@@ -1,20 +1,41 @@
 # Define source and destination directories
-$sourceDirectory = "C:\Users\metha\Documents\Develop"
-$destinationDirectory = "D:\Backup"
+$sourceDirectory = "Hier Pfad zum Backup angeben"
+$destinationDirectory = "HIER Destination Pfad am besten Externe Festplatte benutzen"
+# If i Decide to use Zip for Powershell Commented for now
+# $pathtomodule= ".\7Zip4Powershell"
+$deletionDate = Get-Date.AddDays(+2)
 
 # Create destination directory if it does not exist
 if (-not (Test-Path -Path $destinationDirectory)) {
     New-Item -Path $destinationDirectory -ItemType Directory
 }
 
-# Get current date to create a timestamp for the backup folder
-$dateStamp = Get-Date -Format "yyyyMMdd_HHmmss"
+# Tar the folder Together or Using the 7Zip4Powershell Module dont know yet wait for VM Install
+function Create-Archive {
+    # Get current date to create a timestamp for the backup folder
+    $dateStamp = Get-Date -Format "yyyyMMdd_HHmmss"
+    $tarfile = "$dateStamp_Backup.tar.gz"
+
+    #In Theory with Tar
+   cmd.exe /c "tar -cvzf $sourceDirectory"
+   Move-Item -Path $sourceDirectory\$tarfile -Destination $destinationDirectory 
+}
+
+function Test_Age {
+    # Check if File is Older Than Deletion Date
+    if $tarfile >= $deletionDate {
+        Remove-Item $tarfile
+    }
+    else {
+        Write-Host "TarFile Up to Date no Deletion Required"
+    }
+}
 
 # Create a folder with current date stamp under the destination directory
-$backupFolder = Join-Path -Path $destinationDirectory -ChildPath "Backup_$dateStamp"
-New-Item -Path $backupFolder -ItemType Directory
+# $backupFolder = Join-Path -Path $destinationDirectory -ChildPath "Backup_$dateStamp"
+# New-Item -Path $backupFolder -ItemType Directory
 
 # Copy files from source directory to the backup folder
-Copy-Item -Path $sourceDirectory\* -Destination $backupFolder -Recurse -Force
+# Copy-Item -Path $sourceDirectory\* -Destination $backupFolder -Recurse -Force
 
 Write-Host "Backup completed successfully."
