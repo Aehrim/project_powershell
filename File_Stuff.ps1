721a217207ed8377843b4d1e2b9a7561c665937b
Rafael Hartung <63423283+Aehrim@users.noreply.github.com>
2024-03-05 11:44:35 +0100

    Started the File Stuff Script just need to Figure out what it should actually do

---
+++ File_Stuff.ps1
@@ -0,0 +1,41 @@
+# First of All Powershell is a Pain in The ass Why cant i just use your Typical System Commands instead of these Fancy CMDLETS I would need maybe Half the Code in Linux but thats just me Ranting
+# So Whats the Plan Here? We want Optimization for File Management and Stuff for the New Server
+# So We create an Archival Function that Iterates Through the Whole Drive (System Data Excluded We dont Want to Accidentally Wipe the System do we now?:D) and Compares The Age of the Data to the Age i Set here
+# If the Age is Older than i set here the Archival Function is Triggered which then Archives the Old Files und Moves Them to a Archival Folder  
+# In Case the Age equals The Same Day or Just a Bit Older e.g 1 Day ago we Want to create A Host Message that says All Good Sir or some Shit like That
+
+# Set Policy to Run The Script without Signing
+Set-Executionpolicy Unrestricted -Force
+
+# Set Root and Possible Archive File
+$rootDirectory = "C:\"
+$archivedir = "HERE would be the Path to the Archive Dir"
+
+# Get Current Date for Compare Purposes
+$compareDate = Get-Date
+
+# Create Archivedir if it doesnt exist
+if (-not (Test-Path -Path $archivedir)) {
+    New-Item -Path $archivedir -ItemType Directory
+}
+
+function Archival {
+    # When a File or Directory is Found in Best Case More than one Pack them in an Archive and move them to $archivedir 
+    # if its Only One or 2 Just Copy them and delete the Original if possible
+    
+}
+
+# Archive Data Thats Older than x Days
+function CheckAge {
+    # Iterate Through whole Drive and Exclude System Critical Files
+    $drives = Get-ChildItem -Path $rootDirectory -Recurse -File -Exclude "$rootDirectory\Windows\*"
+
+    foreach ($drive in $drives) {
+
+    }
+
+    if $drives == $compareDate {
+        Write-Host "All Good Files are UptoDate and dont need Archiving"
+    }
+
+}
